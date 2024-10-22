46a93bc5c06cfac0641a2f990bf8e0e1acc4e010
haobo Li <zerobug705@gmail.com>
2024-10-23 00:01:59 +1300

    innit database when running the app

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from models.database import  init_db
+
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return "Hello"
+
+
+
+
+if __name__ == "__main__":
+    init_db()
+    app.run()
