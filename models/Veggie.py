03f4278127a27fb7b6e0a98bceda3a472f56903b
haobo Li <zerobug705@gmail.com>
2024-10-23 02:11:58 +1300

    Refactor and finished the veggie class

--- models/Veggie.py
+++ models/Veggie.py
@@ -1,17 +1,57 @@
-from abc import ABC
+from sqlalchemy import Column, String, Integer, DECIMAL ,FLOAT
+from base import Base
+
+class Veggie(Base):
+	__tablename__ = "veggie"
+	id = Column(Integer, primary_key=True)
+	vegName = Column(String(100)) 
+	pricePerUnit = Column(DECIMAL(2), nullable = True) # unit veggie
+	quantity = Column(Integer, nullable = True) # unit veggie
+	numOfPack = Column(Integer, nullable = True) # pack veggie
+	pricePerPack = Column(DECIMAL(2), nullable = True) # pack veggie
+	weight = Column(FLOAT) # weight veggie
+	pricePerKiol = Column(DECIMAL(2), nullable = True) # weight veggie
+	type = Column(String(50)) # discriminator
 
 
-class Veggie(ABC):
-	def __init__(self,vegName : str ) -> None:
-		super().__init__()
-		self._vegName = vegName
+	__mapper_args__ = {
+        'polymorphic_identity': 'veggie',
+        'polymorphic_on': type
+    }
+
+	def __init__(self, vegName, pricePerUnit=None, quantity=None, numOfPack=None, pricePerPack=None, weight=None, pricePerKiol=None):
+		self.vegName = vegName
+
 	
-	@property
-	def vegName(self):
-		return self._vegName
+class UnitPriceVeggie(Veggie):
+	__mapper_args__ = {
+		'polymorphic_identity': 'unitPriceVeggie',
+	}
+
+	def __init__(self, vegName, pricePerUnit, quantity):
+		super().__init__(vegName)
+		self.pricePerUnit = pricePerUnit
+		self.quantity = quantity
 	
-	@vegName.setter
-	def vegName(self, name):
-		self._vegName = name
+	def __str__(self):
+		return f"{self.vegName} {self.type} {self.pricePerUnit} {self.quantity}"
+	
+class WeightedVeggie(Veggie):
+	__mapper_args__ = {
+		'polymorphic_identity': 'weightedVeggie',
+	}
+
+	def __init__(self, vegName,  weight, pricePerKiol):
+		super().__init__(vegName)
+		self.weight = weight
+		self.pricePerKiol = pricePerKiol
+
+	def __str__	(self):
+		return f"{self.vegName} {self.weight} {self.pricePerKiol}"
+
+
+
+
 		
+
 
