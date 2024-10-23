111d8ad822e16347494fb911bc28374d94ff5fd0
haobo Li <Kevin.Li@lincolnuni.ac.nz>
2024-10-24 00:32:12 +1300

    Update CreditCardPayment.py

---
+++ models/CreditCardPayment.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Integer, DECIMAL ,FLOAT,ForeignKey,DATETIME
+from base import Base
+
+class CreditCardPayment(Base):
+	# mapping 
+	paymentId = Column(Integer, ForeignKey("payment.paymentId"), primary_key=True)
+	cardExpireDate = Column(DATETIME)
+	cardType = Column(String(50))
+
+	def __init__(self,paymentId, cardExpireDate,cardType):
+		self.paymentId = paymentId
+		self.cardExpireDate = cardExpireDate
+		self.cardType = cardType
+
