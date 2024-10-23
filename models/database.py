24f0d9c2330960af392776421f14d9adddbcabad
haobo Li <zerobug705@gmail.com>
2024-10-23 22:54:30 +1300

    Update database.py

--- models/database.py
+++ models/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from . import Veggie
+from . import Person
+from .import Staff
 from base import Base, session
+from datetime import date
 
 engine = create_engine("mysql+mysqlconnector://root:password@localhost:3306/veggie_shop",echo=True)
 
@@ -10,7 +13,11 @@ def init_db():
 	Base.metadata.create_all(bind=engine)
 
 	veggie = Veggie.Veggie("bok choy")
-	print(veggie.vegName)
+	# person = Person.Person()
+	today =  date.today()
+	staff = Staff.Staff("kevin", "li", "1234", "kevinli",1, today, "hello-dep", "abcd",None, None)
 	session.add(veggie)
+	# session.add(person)
+	session.add(staff)
 	session.commit()
 	
