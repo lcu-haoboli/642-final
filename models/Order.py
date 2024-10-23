11c409ec33ae36c60f14fc79a2e0df3e15c7c263
haobo Li <zerobug705@gmail.com>
2024-10-23 23:58:50 +1300

    add order class

--- models/Order.py
+++ models/Order.py
@@ -1,44 +1,17 @@
+from sqlalchemy import Column, String, Integer, DECIMAL ,FLOAT,ForeignKey,DATETIME
+from base import Base
 
-class Order:
+class Order(Base):
+	# mapping
+	orderId = Column(Integer, primary_key=True)
+	orderCustomer = Column(Integer, ForeignKey=("customer.custId"))
+	orderDate = Column(DATETIME)
+	orderNumber = Column(Integer)
+	orderStatus = Column(String(50))
+
 	def __init__(self, orderCustomer, orderDate,orderNumber,orderStatus) -> None:
-		self._orderCustomer = orderCustomer
-		self._orderDate = orderDate
-		self.orderNumber = orderNumber
-		self._orderStatus = orderStatus
-	
-
-	@property
-	def orderCustomer(self):
-		return self._orderCustomer
-	
-	@orderCustomer.setter
-	def orderCustomer(self, customer):
-		self._orderCustomer = customer
-	
-	@property
-	def orderDate(self):
-		return self._orderDate
-
-	@orderDate.setter
-	def orderDate(self, orderDate):
+		self.orderCustomer = orderCustomer
 		self.orderDate = orderDate
+		self.orderNumber = orderNumber
+		self.orderStatus = orderStatus
 	
-	@property
-	def orderNumber(self):
-		return self._orderNumber
-	
-	@orderNumber.setter
-	def orderNumber(self, number):
-		self._orderNUmber = number
-	
-	@property
-	def orderStatus(self):
-		return self._orderStatus
-	
-	@orderStatus.setter
-	def orderStatus(self,value):
-		self._orderStatus = value
-
-
-
-		
