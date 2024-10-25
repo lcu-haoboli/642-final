c4e0b1cfd764e173c8c5ed2dd18a4662263a20ef
haobo Li <Kevin.Li@lincolnuni.ac.nz>
2024-10-26 03:07:21 +1300

    add router

--- controllers/veggie.py
+++ controllers/veggie.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, flash, redirect, url_for, jsonify, request,session,render_template
-
+from base import Session
+from models.Veggie import Veggie
 veggie_bp = Blueprint("veggie", __name__, static_folder="static", template_folder="templates/products")
+
+db_session = Session()
 
 @veggie_bp.route("/")
 def veggie():
-	return render_template("products/veggie.html")
+	# query all veggie
+	veggies = db_session.query(Veggie).all()
+	# for veggie in veggies:
+		# print(veggie)
+	return render_template("products/veggie.html", veggies=veggies)
+	# return render_template("products/veggie.html", veggie=[])
