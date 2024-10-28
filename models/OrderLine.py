8d490ea5eebb4ddf6e0dea071c3763e3341a6b9f
haobo Li <Kevin.Li@lincolnuni.ac.nz>
2024-10-29 08:47:01 +1300

    Update OrderLine.py

--- models/OrderLine.py
+++ models/OrderLine.py
@@ -1,16 +1,18 @@
 from base import Base
 from sqlalchemy import Column, String, Integer, DECIMAL ,FLOAT,ForeignKey
 
-class OrderLine:
+class OrderLine(Base):
 	# mapping
 	__tablename__ = "order_line"
 	orderLineId = Column(Integer, primary_key=True)
 	orderId = Column(Integer,ForeignKey("order.orderId"))
 	veggieId = Column(Integer, nullable=True)
 	premadeBoxId = Column(Integer, nullable=True)
+	orderQuantity = Column(Integer)
 
-	def __init__(self, orderId, veggieId, premadeBoxId) -> None:
+	def __init__(self, orderId, veggieId, premadeBoxId,orderQuantity) -> None:
 		self.orderId = orderId
 		self.veggieId = veggieId
 		self.premadeBoxId = premadeBoxId
+		self.orderQuantity = orderQuantity
 	
