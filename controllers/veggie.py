7e21260031dc333bd8bba58e40c9c553f334db25
haobo Li <Kevin.Li@lincolnuni.ac.nz>
2024-10-29 08:42:44 +1300

    update cart item when add veggie click

--- controllers/veggie.py
+++ controllers/veggie.py
@@ -1,15 +1,18 @@
-from flask import Blueprint, flash, redirect, url_for, jsonify, request,session,render_template
+from flask import Blueprint, flash, redirect, url_for, jsonify, request,session,render_template,session,json
 from base import Session
 from models.Veggie import Veggie
 veggie_bp = Blueprint("veggie", __name__, static_folder="static", template_folder="templates/products")
 
 db_session = Session()
 
-@veggie_bp.route("/")
+@veggie_bp.route("/", methods = ["GET", "POST"])
 def veggie():
-	# query all veggie
 	veggies = db_session.query(Veggie).all()
-	# for veggie in veggies:
-		# print(veggie)
+	if request.method == 'POST':
+		data = request.get_json()
+		cartItems = data["cartItems"]
+		session['cartItems'] = json.loads(cartItems)
+		print(session['cartItems'])
+		return render_template("products/veggie.html", veggies=veggies)
 	return render_template("products/veggie.html", veggies=veggies)
-	# return render_template("products/veggie.html", veggie=[])
+	
