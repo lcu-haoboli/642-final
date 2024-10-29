e12fd2fbd5dac0c61cb3948bbc2f4eb714a70d8d
lcu-haoboli <kevin.li@lincolnuni.ac.nz>
2024-10-30 03:49:16 +1300

    Update login.py

--- controllers/login.py
+++ controllers/login.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, flash, redirect, url_for, jsonify, request,session,render_template
 from base import Session
 from models.Person import Person
+from models.Customer import Customer
+from models.CorporateCustomer import CorporateCustomer
 
 login_bp = Blueprint("login", __name__, static_folder="static", template_folder="templates")
 db_session = Session()
@@ -20,6 +22,14 @@ def login():
 				session['username'] = username_to_find
 				session['logged_in'] = True
 				session['userType'] = userType
+				if userType == "customer":
+					cust = db_session.query(Customer).filter(Customer.custId == person.id).first()
+					session["maxOwning"] = cust.maxOwning
+					session["custBalance"] = cust.custBalance
+				if userType == "corporateCustomer":
+					corp = db_session.query(CorporateCustomer).filter(CorporateCustomer.corpCustId == person.id).first()
+					session["minBalance"] = corp.minBalance
+					session["maxCredit"] = corp.maxCredit
 				flash("Login successful!", "success")
 				return redirect(url_for("home"))
 			else:
