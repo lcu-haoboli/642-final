ad0b14cb17f521e4f6d7967827cd9375b047517d
lcu-haoboli <kevin.li@lincolnuni.ac.nz>
2024-10-30 04:37:50 +1300

    Update staff.py

--- controllers/staff.py
+++ controllers/staff.py
@@ -1,65 +1,58 @@
 from flask import Blueprint, flash, redirect, url_for, jsonify,request,render_template,session
+from base import Session
+from models.Order import Order
+from models.Person import Person
+from models.Payment import Payment
+from models.Payment import CreditCardPayment
+from models.Payment import DebitCardPayment
+from models.OrderLine import OrderLine
+from datetime import date
+from models.Customer import Customer
+from sqlalchemy.sql import text
+db_session = Session()
+
 
 staff_bp = Blueprint("staff", __name__, static_folder="static", template_folder="templates/staff")
 
-
-@staff_bp.route("/dashboard", methods=['GET', 'POST'])
+@staff_bp.route("/")
 def dashboard():
-   return render_template("global/account_dashboard.html")
+	if session["userType"] != "staff" or session['logged_in'] != True:
+		return redirect(url_for("login.login"))
+	return render_template("staff/dashboard.html")
+	
 
-@staff_bp.route("/inventory_management")
-def inventory_management():
-   try:
-      pass
-   except Exception as e:
-      print("@staff.route(/inventory_management): %s", e)
-      return []
-   return render_template("staff/inventory_management.html")
+@staff_bp.route("/all_customers")
+def allCustomer():
+	# query db
+	query = text("select c.*, p.* from customer c left join person p on c.custId = p.id")
+	result = db_session.execute(query).all()
+	cust_info = []
+	if result:
+		for cust in result:
+			custInfo = {
+					"id" : cust[0],
+					"address" : cust[1],
+					"custBalance" : cust[2],
+					"maxOwning" : cust[3],
+					"firstname" : cust[5],
+					"lastname" : cust[6],
+					"username" : cust[8]
+					}
+			cust_info.append(custInfo)
+	return render_template("staff/all_customers.html", customers = cust_info)
 
-@staff_bp.route("/product_with_inventory", methods=["GET"])
-def product_with_inventory():
-   product = {
-      "product_name": "",
-      "product_description": "",
-      "product_price": "",
-      "product_image": "",
-      "product_status": "",
-      "product_inventory_id": "",
-      "product_inventory": 0
-   }
-   try:
-      product_id = request.args.get('product_id')
-      cursor = getCursor()
-      sql_query = get_product_with_inventory()
-      cursor.execute(sql_query, (product_id, ))
-      result = cursor.fetchone()
-      product = {
-         "product_name": result[0],
-         "product_description": result[1],
-         "product_price": result[2],
-         "product_image": result[3],
-         "product_status": result[4],
-         "product_inventory_id": result[5],
-         "product_inventory": result[6]
-      }
-      return product
-   except Exception as e:
-      print("staff/product_with_inventory : %e ",e)
-      return product
+@staff_bp.route("/all_orders")
+def allOrder():
+	# query
+	orders_custs = db_session.query(Order,Customer).join(Customer).all()
+	list_orders_custs = []
+	for order_cust in orders_custs:
+		order = order_cust[0].__dict__
+		cust = order_cust[1].__dict__
+		list_orders_custs.append({**order, **cust})
+	print(list_orders_custs)
+	for item in list_orders_custs:
+		print(item)
+	return render_template("staff/all_orders.html", orders = list_orders_custs)
 
-@staff_bp.route("/update_product_inventory", methods=["POST"])
-def update_product_inventory():
-   try:
-      inventory_id = request.json.get('inventory_id')
-      new_inventory = request.json.get('new_inventory')
-    #   cursor = getCursor()
-    #   sql_query = update_product_inventory_query()
-    #   cursor.execute(sql_query, (new_inventory, inventory_id))
-    #   cursor.close()
-      return jsonify({'message': 'Inventory Updated successfully'}), 200
-   except Exception as e:
-    #   cursor.close()
-      print("@staff.route(/update_product_inventory): %s", e)
-      return jsonify({'message': 'Inventory Updated Error'}), 400
-
-
+	
